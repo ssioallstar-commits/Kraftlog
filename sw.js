d2fc0feba0a6065c0a6afc488ac3598c08063a41
ssioallstar-commits <ssioallstar@gmail.com>
2026-09-25 14:49:12 +0200

    Aktualisiere Kraftlog Offline-Cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kraftlog-v3';
+const CACHE = 'kraftlog-v5';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting()));
@@ -13,6 +13,8 @@ self.addEventListener('fetch', event => {
     return response;
   })));
 });
+
+
 
 
 
