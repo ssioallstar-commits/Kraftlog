9caef4ac066fdb3a23c9fcc94cf019d7d2f36af4
ssioallstar-commits <ssioallstar@gmail.com>
2026-09-25 15:56:16 +0200

    Refresh Kraftlog offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kraftlog-v5';
+const CACHE = 'kraftlog-v6';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting()));
